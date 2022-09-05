557878b4cfcfe5d97add2904a270be8a6ad4730d
Brian Sokas <brian.sokas@gmail.com>
2022-09-05 15:42:19 -0400

    Restarting with iOS-specific project and wiping out the React Native files

---
+++ BloodPressureCop/BloodPressureCopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloodPressureCopApp.swift
+//  BloodPressureCop
+//
+//  Created by Brian Sokas on 9/5/22.
+//
+
+import SwiftUI
+
+@main
+struct BloodPressureCopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
